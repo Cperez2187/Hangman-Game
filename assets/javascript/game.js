546be627458a9ed30db85a63fe304b30ceac433f
cperez2187 <cperez2187@gmail.com>
2017-04-27 11:54:30 -0700

    updated JS file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 //	Create playGame object
 //**************************
 
-var playGame = {
+var hangman = {
 
 	//~~~~~~~~~~~~~~
 	// Properties
@@ -25,24 +25,44 @@ var playGame = {
 		var formattedWord = "";
 
 		for (var i = 0; i < gameLetters.length; i++) {
-			formattedWord + = gameLetters[i];
+			formattedWord += gameLetters[i];
 
 			if (i != letters.length - 1) {
 				formattedWord += " ";
 			}
 		}
 		return formattedWord;
-	}
+	},
 
 	// Updates and displays "word-display" in html
 	outputWordLetters: function() {
 		document.getElementById("word-display").innerHTML = gameFormatWord(gameLetters);
-	}
+	},
 
 	// Updates and displays guessed letters in html
 	outputUsedLetters: function() {
 		document.getElementById("guessed").innerHTML = gameFormatWord(usedLetters);
-	}
+	},
+
+	// Check if user has guessed all the correct letters
+	checkWin: function() {
+		for (var i = 0; i < gameLetters.length; i++) {
+			if (gameLetters[i] === "_") {
+				return false; // Still missing letters
+			}
+		}
+		return true;  // All letters have been guessed
+	},
+
+	// Checks if the key is an actual letter
+	isLetter: function(key) {
+		let invalidKeys = ["Enter", "Meta", "Control", "Alt", "CapsLock", "Shift",
+		 "ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp"];
+
+		let alphabet = /^[a-zA-Z]+$/i;
+
+		return alphabet.test(key);
+	},
 
 }
 
