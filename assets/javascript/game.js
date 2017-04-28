15ee605f55699d35dc97180e74301d44009ab34d
cperez2187 <cperez2187@gmail.com>
2017-04-28 14:10:15 -0700

    still updating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,18 @@
-//*************************
-//	Global variables
-//*************************
+//**********************************
+//	Global variables && Functions
+//**********************************
 
 var gameOver = false;
+
+// Checks if the key is an actual letter
+function isLetter(key) {
+	let invalidKeys = ["Enter", "Meta", "Control", "Alt", "CapsLock", "Shift",
+	 "ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp"];
+
+	let alphabet = /^[a-zA-Z]+$/i;
+
+	return alphabet.test(key);
+}
 
 //**************************
 //	hangman object
@@ -33,8 +43,8 @@ var hangman = {
 	gameFormatWord: function(letters) {
 		let formattedWord = "";
 
-		for (var i = 0; i < this.gameLetters.length; i++) {
-			formattedWord += this.gameLetters[i];
+		for (var i = 0; i < letters.length; i++) {
+			formattedWord += letters[i];
 
 			if (i != letters.length - 1) {
 				formattedWord += " ";
@@ -63,16 +73,6 @@ var hangman = {
 		return true;  // All letters have been guessed
 	},
 
-	// Checks if the key is an actual letter
-	isLetter: function(key) {
-		let invalidKeys = ["Enter", "Meta", "Control", "Alt", "CapsLock", "Shift",
-		 "ArrowLeft", "ArrowRight", "ArrowDown", "ArrowUp"];
-
-		let alphabet = /^[a-zA-Z]+$/i;
-
-		return alphabet.test(key);
-	},
-
 	// Grabs a random word from wordBag
 	randomWord: function() {
 		this.word = this.wordBag[Math.floor(Math.random() *this.wordBag.length)];
@@ -80,6 +80,23 @@ var hangman = {
 	},
 
 	playGame: function(key) {
+		// If game is not over and user pressed a letter
+		if (!gameOver && isLetter(key)) {
+			// If Letter is in the word
+			if (this.word.includes(key)) {
+				//replace corresponding underscore in gameLetters with letter
+				for (var i = this.word.indexOf(key); i < this.word.length; i++) {
+					if (this.word.charAt(i) === key) {
+						this.gameLetters[i] = String(key);
+					}
+				}
+			}
+
+
+		}
+		else if (gameOver) {
+
+		}
 
 	}
 };
@@ -91,6 +108,7 @@ var hangman = {
 do {
 	// Sets 'word' attribute = random word from 'wordBag'
 	hangman.randomWord();
+	console.log("Word: " + hangman.word);
 
 	// Fills 'gameLetters' array with underscores representing missing letters of word
 	for (var i = 0; i < hangman.word.length; i++) {
@@ -99,10 +117,13 @@ do {
 
 	// Display empty word spaces
 	hangman.outputWordLetters();
+	hangman.outputUsedLetters();
+	console.log("usedLetters: " + hangman.usedLetters);
 
 	document.onkeydown = function keyUp(event) {
 
 	}
+	gameOver = true;
 } while (gameOver === false);
 
 
